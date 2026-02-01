c1713c8ec1d0b468361cba6eecb91f6634db1c9f
1torres1 <155696400+1t0rres1@users.noreply.github.com>
2026-02-01 15:43:05 +0800

    upload the files
    upload the files

---
+++ SAP NetWeaver 750_7 RFC SDK/sap_nwrfcsdk_75_7_documentation/html/search/enumvalues_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['communication_5ffailure',['COMMUNICATION_FAILURE',['../sapnwrfc_8h.html#ga497758475b7ea26a55501399ec03a2e6a41d6abb3e5d6782202ec19b2c9572c97',1,'sapnwrfc.h']]]
+];
---
+++ SAP NetWeaver 750_7 RFC SDK/sap_nwrfcsdk_75_7_documentation/html/search/enumvalues_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['logon_5ffailure',['LOGON_FAILURE',['../sapnwrfc_8h.html#ga497758475b7ea26a55501399ec03a2e6a3bdd035e157f9618a0312f7ebac1919f',1,'sapnwrfc.h']]]
+];
